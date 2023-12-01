3872bd61e896f992d47bb67dba04bce272c29c9d
ihebcharfeddine <charfeddine.iheb@enis.tn>
2023-12-01 09:17:32 +0100

    interface of review , add review

--- js/main.js
+++ js/main.js
@@ -68,4 +68,32 @@
       },
     },
   });
+  // Testimonials carousel
+  $(".review-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    dots: false,
+    loop: false,
+    nav : true,
+    navText : [
+        '<i class="bi bi-chevron-left"></i>',
+        '<i class="bi bi-chevron-right"></i>'
+    ],
+    responsive: {
+        0:{
+            items:1
+        },
+        576:{
+            items:1
+        },
+        768:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+});
+
 })(jQuery);
